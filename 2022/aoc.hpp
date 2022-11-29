6c1974102e17349cac09437e8e915983072446ba
Caleb Epstein <github-bklyn@sneakemail.com>
2022-11-29 11:26:06 -0500

    2022 prep

---
+++ 2022/aoc.hpp
@@ -0,0 +1,27 @@
+// AOC C++ helper functions
+
+#pragma once
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_set>
+#include <utility>
+
+std::unique_ptr<std::istream> input(int day) {
+    std::string filename = std::to_string(day) + ".txt";
+    auto ptr = std::make_unique<std::ifstream>(filename);
+    if (!*ptr) {
+        throw std::runtime_error("Unable to read " + filename);
+    }
+    return ptr;
+}
